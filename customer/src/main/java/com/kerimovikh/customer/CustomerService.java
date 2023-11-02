674cbf9a5f365bb05774014c6b404f833422d807
Islam Kerimov <kih@irbis.plus>
2023-11-02 11:42:57 +0400

    add fraud module

--- customer/src/main/java/com/kerimovikh/customer/CustomerService.java
+++ customer/src/main/java/com/kerimovikh/customer/CustomerService.java
@@ -2,11 +2,15 @@ package com.kerimovikh.customer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registrationCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -14,6 +18,16 @@ public class CustomerService {
             .lastName(request.lastName())
             .email(request.email())
             .build();
-        customerRepository.save(customer);
+
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+            "http://localhost:8081/api/v1/fraud-check/{customerId}",
+            FraudCheckResponse.class,
+            customer.getId());
+        if (fraudCheckResponse.getIsFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
     }
 }
