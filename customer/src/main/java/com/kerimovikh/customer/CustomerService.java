5023c20ae62c902f5f456c6ad4f01a8791af1416
Islam Kerimov <kih@irbis.plus>
2023-11-10 14:20:11 +0400

    add eureka service

--- customer/src/main/java/com/kerimovikh/customer/CustomerService.java
+++ customer/src/main/java/com/kerimovikh/customer/CustomerService.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
@@ -22,7 +20,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-            "http://localhost:8081/api/v1/fraud-check/{customerId}",
+            "http://FRAUD/api/v1/fraud-check/{customerId}",
             FraudCheckResponse.class,
             customer.getId());
         if (fraudCheckResponse.getIsFraudster()) {
